67908320e9e0032d18338a6fe3258fde6c186adc
Kevin Thompson <kevin.thompson@theautomaters.com>
2012-08-30 09:19:51 -0700

    Added a way to deal with Exceptions in the TimeoutDispatcher.

--- Automaters.Core/Timers/TimeoutDispatcher.cs
+++ Automaters.Core/Timers/TimeoutDispatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -32,7 +33,19 @@ namespace Automaters.Core.Timers
         /// <returns></returns>
         public IDisposable Add(Action tick, TimeSpan timeout)
         {
-            var timeoutObj = new TimeoutObject(this, tick, timeout, TimeSpan.Zero);
+            return Add(tick, null, timeout);
+        }
+
+        /// <summary>
+        /// Adds the specified timeout action.
+        /// </summary>
+        /// <param name="tick">The tick.</param>
+        /// <param name="exceptionHandler">the exception handler </param>
+        /// <param name="timeout">The timeout.</param>
+        /// <returns></returns>
+        public IDisposable Add(Action tick, Action<Exception> exceptionHandler, TimeSpan timeout)
+        {
+            var timeoutObj = new TimeoutObject(this, tick, exceptionHandler, timeout, TimeSpan.Zero);
             this.AddInternal(timeoutObj);
             return timeoutObj;
         }
@@ -45,22 +58,35 @@ namespace Automaters.Core.Timers
         /// <returns></returns>
         public IDisposable AddRepeating(Action tick, TimeSpan interval)
         {
-            return this.AddRepeating(tick, interval, TimeSpan.Zero);
+            return AddRepeating(tick, null, interval);
         }
 
         /// <summary>
         /// Adds the specified repeating timeout action.
         /// </summary>
         /// <param name="tick">The tick.</param>
+        /// <param name="exceptionHandler">The exception handler </param>
+        /// <param name="interval">The interval.</param>
+        /// <returns></returns>
+        public IDisposable AddRepeating(Action tick, Action<Exception> exceptionHandler, TimeSpan interval)
+        {
+            return this.AddRepeating(tick, exceptionHandler, interval, TimeSpan.Zero);
+        }
+
+        /// <summary>
+        /// Adds the specified repeating timeout action.
+        /// </summary>
+        /// <param name="tick">The tick.</param>
+        /// <param name="exceptionHandler">The exception handler </param>
         /// <param name="interval">The interval.</param>
         /// <param name="initialDelay">The initial delay.</param>
         /// <returns></returns>
-        public IDisposable AddRepeating(Action tick, TimeSpan interval, TimeSpan initialDelay)
+        public IDisposable AddRepeating(Action tick, Action<Exception> exceptionHandler, TimeSpan interval, TimeSpan initialDelay)
         {
             if (initialDelay <= TimeSpan.Zero)
                 initialDelay = interval;
 
-            var timeoutObj = new TimeoutObject(this, tick, initialDelay, interval);
+            var timeoutObj = new TimeoutObject(this, tick, exceptionHandler, initialDelay, interval);
             this.AddInternal(timeoutObj);
             return timeoutObj;
         }
@@ -164,7 +190,7 @@ namespace Automaters.Core.Timers
                         break;
 
                     // Get our next timeout object
-                    nextTimeout = this.TimeoutObjects.Where(obj => !obj.IsExecuting).OrderBy(obj => obj.NextTick).FirstOrDefault();
+                    nextTimeout = this.TimeoutObjects.Where(o => !o.IsExecuting).OrderBy(o => o.NextTick).FirstOrDefault();
 
                     // If we didn't find anything that means all timeouts are executing still
                     // So just wait indefinitely until someone wakes us up
@@ -188,33 +214,56 @@ namespace Automaters.Core.Timers
                         nextTimeout.Dispose();
                 }
 
-                // If we found the next timeout to trigger then trigger it
-                if (nextTimeout != null)
+                // Trigger the next timeout
+                // Create a local variable to avoid issues with the closure
+                var obj = nextTimeout;
+                obj.IsExecuting = true;
+                ThreadPool.QueueUserWorkItem(data =>
                 {
-                    // Create a local variable to avoid issues with the closure
-                    TimeoutObject obj = nextTimeout;
-                    obj.IsExecuting = true;
-                    ThreadPool.QueueUserWorkItem((data) =>
+                    // Execute the tick and then figure out the next tick time
+                    try
                     {
-                        // Execute the tick and then figure out the next tick time
                         obj.Tick();
-                        obj.NextTick = DateTime.Now.Add(obj.Interval);
-                        obj.IsExecuting = false;
+                    }
+                    catch (Exception exception)
+                    {
+                        Break();
 
-                        // If this is a repeating job then we should probably notify our thread
-                        // That we updated the next tick time so it knows how long to wait
-                        if (obj.Interval != TimeSpan.Zero)
+                        try
                         {
-                            lock (this.TimeoutObjects)
-                            {
-                                // Since some time may have passed make sure we're still valid before signaling
-                                if (this.TimeoutObjects.Contains(obj))
-                                    Monitor.PulseAll(this.TimeoutObjects);
-                            }
+                            if (obj.ExceptionHandler != null)
+                                obj.ExceptionHandler(exception);
                         }
-                    });
-                }
+                        catch(Exception)
+                        {
+                            //if there was an exception in the exception handler we will just silence it.
+                            Break();
+                        }
+                    }
+
+                    obj.NextTick = DateTime.Now.Add(obj.Interval);
+                    obj.IsExecuting = false;
+
+                    // If this is a repeating job then we should probably notify our thread
+                    // That we updated the next tick time so it knows how long to wait
+                    if (obj.Interval == TimeSpan.Zero) 
+                        return;
+
+                    lock (this.TimeoutObjects)
+                    {
+                        // Since some time may have passed make sure we're still valid before signaling
+                        if (this.TimeoutObjects.Contains(obj))
+                            Monitor.PulseAll(this.TimeoutObjects);
+                    }
+                });
             }
+        }
+
+        [DebuggerHidden]
+        private static void Break()
+        {
+            if (Debugger.IsAttached)
+                Debugger.Break();
         }
 
         #endregion
@@ -243,10 +292,11 @@ namespace Automaters.Core.Timers
         protected class TimeoutObject : IDisposable
         {
 
-            public TimeoutObject(TimeoutDispatcher dispatcher, Action tick, TimeSpan delay, TimeSpan interval)
+            public TimeoutObject(TimeoutDispatcher dispatcher, Action tick, Action<Exception> exceptionHandler, TimeSpan delay, TimeSpan interval)
             {
                 this.Dispatcher = dispatcher;
                 this.Tick = tick;
+                this.ExceptionHandler = exceptionHandler;
                 this.NextTick = DateTime.Now.Add(delay);
                 this.Interval = interval;
             }
@@ -277,6 +327,18 @@ namespace Automaters.Core.Timers
             public Action Tick
             {
                 get;
+                protected set;
+            }
+
+            /// <summary>
+            /// Gets or sets the exception action.
+            /// </summary>
+            /// <value>
+            /// The exception handler.
+            /// </value>
+            public Action<Exception> ExceptionHandler
+            {
+                get; 
                 protected set;
             }
 
